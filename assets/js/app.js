9082ee2493f173d927fe78f6908cb53eb600bb3a
viviana <dmpviviana@gmail.com>
2018-07-12 17:31:03 -0400

    login con facebook

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,27 @@
 // $(document).ready(function(){
 //     $('.sidenav').sidenav();
 //   });
+
+// creamos un nuevo objeto con firebase
+// utilizamos setcustomparametres , display popup, con eso le decimos  firebase que hagas el login con facebook en un popup
+// ahora llamamos a firebase.auth , utilizamos singwithpopup, introducimos el provider y luego esa funcion retorna una promesa
+// then para obtener el resultado y catch para los errores
+// agregamos en firebase un dominio autorizado , 127.0.0.1 es el 200 ok
+
+//Login con Facebook
+function facebookLoginWithFirebase(){
+    const provider = new firebase.auth.FacebookAuthProvider(); // creamos un nuevo objeto 
+
+    provider.setCustomParameters({ // le decimos que haga un login con facebook y enlace un popup
+        'display' : 'popup'
+    });
+
+    firebase.auth().signInWithPopup(provider)
+        .then(()=>{
+            console.log("Login con facebook exitoso");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });
+}
