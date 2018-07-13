b8e70027881b578748199015191e590b79d6137a
Valeria Cossio <valeriacossio5@gmail.com>
2018-07-12 21:18:00 -0400

    registro

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,17 @@
 // $(document).ready(function(){
 //     $('.sidenav').sidenav();
 //   });
+
+
+// function registerWithFirebase() {
+//     const emailValue = email.value;
+//     const passwordValue = password.value;
+
+//     firebase.auth().createrUserWithEmailAndPassword(emailValue, passwordValue)
+//     .then(() => {
+//         console.log("Usuario creado con éxito");
+//     }).catch((error) => {
+//         console.log("Error de firebase > Código > "+error.code);
+//         console.log("Error de firebase > Mensaje > "+error.message);
+//     });
+// }
